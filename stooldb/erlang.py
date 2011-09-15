f26a90b820033dfced3ebcc5a5b7597267c0b9cb
Keith Batten <kbatten@gmail.com>
2011-09-15 16:57:57 -0700

    combine the size query with data query

--- stooldb/erlang.py
+++ stooldb/erlang.py
@@ -1,10 +1,8 @@
-"""Python implementation of Erlang builtins"""
+"""Python implementation of Erlang builtins for reading and writing data files"""
 
 __all__=["binary_to_term"]
 
 import struct
-
-SIZE_BLOCK = 4096
 
 ATOM_CACHE_REF = 82
 SMALL_INTEGER_EXT = 97
@@ -30,38 +28,100 @@ EXPORT_EXT = 113
 BIT_BINARY_EXT = 77
 NEW_FLOAT_EXT = 70
 
+# recursive function
+# returns data, length
+# data is the python representation of the erlang binary
+# length is the number of bytes of the erlang binary
+def _binary_to_term_len(bin):
+##    # strip 131 header if it exists
+##    if ord(bin[0]) == 131:
+##        bin = bin[1:]
 
-# return the length of a binary term in bytes
-# incuding tag and any header information
-def _get_binary_size(bin):
     tag = ord(bin[0])
     data = bin[1:]
 
-    if tag == SMALL_TUPLE_EXT:
+    if tag == 131:
+        return _binary_to_term_len(data)
+    elif tag == ATOM_CACHE_REF:
+        return struct.unpack(">B", data[0])[0], 1 + 1
+    elif tag == SMALL_INTEGER_EXT:
+        return struct.unpack(">B",data[0])[0], 1 + 1
+    elif tag == INTEGER_EXT:
+        return struct.unpack(">I",data[0:4])[0], 1 + 4
+    elif tag == FLOAT_EXT:
+        return float(data[0:31]), 1 + 31
+    elif tag == ATOM_EXT:
+        length = struct.unpack(">H",data[0:2])[0]
+        return data[2:2+length], 1 + 2 + length
+    elif tag == REFERENCE_EXT:
+        node, offset = _binary_to_term_len(data)
+        return (node,
+                struct.unpack(">I",data[offset:offset+4])[0],
+                struct.unpack(">B",data[offset+4:offset+4+1])[0]), 1 + offset + 4 + 1
+    elif tag == PORT_EXT:
+        node, offset = _binary_to_term_len(data)
+        return (node,
+                struct.unpack(">I",data[offset:offset+4])[0],
+                struct.unpack(">B",data[offset+4:offset+4+1])[0]), 1 + offset + 4 + 1
+    elif tag == PID_EXT:
+        node, offset = _binary_to_term_len(data)
+        return (node,
+                struct.unpack(">I",data[offset:offset+4])[0],
+                struct.unpack(">I",data[offset+4:offset+4+4])[0],
+                struct.unpack(">B",data[offset+4+4:offset+4+4+1])[0]), 1 + offset + 4 + 4 + 1
+    elif tag == SMALL_TUPLE_EXT:
         arity = struct.unpack(">B",data[0])[0]
+        elements = []
         offset = 1
         for i in range(arity):
-            offset += _get_binary_size(data[offset:])
-        return 1 + offset
-    if tag == ATOM_EXT:
-        length = struct.unpack(">H",data[0:2])[0]
-        return 1 + 2 + length
-    if tag == SMALL_INTEGER_EXT:
-        return 1 + 1
-    if tag == INTEGER_EXT:
-        return 1 + 4
-    if tag == NIL_EXT:
-        return 1
-    if tag == LIST_EXT:
+            element, length = _binary_to_term_len(data[offset:])
+            elements.append(element)
+            offset += length
+        return tuple(elements), 1 + offset
+    elif tag == LARGE_TUPLE_EXT:
+        arity = struct.unpack(">I",data[0:4])[0]
+        elements = []
+        offset = 4
+        for i in range(arity):
+            element, length = _binary_to_term_len(data[offset:])
+            elements.append(element)
+            offset += length
+        return tuple(elements), 4 + offset
+    elif tag == NIL_EXT:
+        return None, 1
+    elif tag == STRING_EXT:
+        pass
+    elif tag == LIST_EXT:
         length = struct.unpack(">I",data[0:4])[0]
+        elements = []
         offset = 4
         # length + 1 because of tail
-        for i in range(length + 1):
-            offset += _get_binary_size(data[offset:])
-        return 1 + offset
-    if tag == BINARY_EXT:
+        for i in range(length+1):
+            element, length = _binary_to_term_len(data[offset:])
+            elements.append(element)
+            offset += length
+        return elements, 1 + offset
+    elif tag == BINARY_EXT:
         length = struct.unpack(">I",data[0:4])[0]
-        return 1 + 4 + length
+        return data[4:4+length], 1 + 4 + length
+    elif tag == SMALL_BIG_EXT:
+        pass
+    elif tag == LARGE_BIG_EXT:
+        pass
+    elif tag == NEW_REFERENCE_EXT:
+        pass
+    elif tag == SMALL_ATOM_EXT:
+        pass
+    elif tag == FUN_EXT:
+        pass
+    elif tag == NEW_FUN_EXT:
+        pass
+    elif tag == EXPORT_EXT:
+        pass
+    elif tag == BIT_BINARY_EXT:
+        pass
+    elif tag == NEW_FLOAT_EXT:
+        pass
 
     raise Exception("Unknown erlang term type (tag: {0})".format(tag))
 
@@ -70,41 +130,4 @@ def _get_binary_size(bin):
 def binary_to_term(bin):
     """Decode erlang binary data into python values"""
 
-    # strip 131 header
-    if ord(bin[0]) == 131:
-        bin = bin[1:]
-
-    tag = ord(bin[0])
-    data = bin[1:]
-
-    if tag == SMALL_TUPLE_EXT:
-        arity = struct.unpack(">B",data[0])[0]
-        elements = []
-        offset = 1
-        for i in range(arity):
-            elements.append(binary_to_term(data[offset:]))
-            offset += _get_binary_size(data[offset:])
-        return tuple(elements)
-    if tag == ATOM_EXT:
-        length = struct.unpack(">H",data[0:2])[0]
-        return data[2:2+length]
-    if tag == SMALL_INTEGER_EXT:
-        return struct.unpack(">B",data[0])[0]
-    if tag == INTEGER_EXT:
-        return struct.unpack(">I",data[0:4])[0]
-    if tag == NIL_EXT:
-        return None
-    if tag == LIST_EXT:
-        length = struct.unpack(">I",data[0:4])[0]
-        elements = []
-        offset = 4
-        # length + 1 because of tail
-        for i in range(length+1):
-            elements.append(binary_to_term(data[offset:]))
-            offset += _get_binary_size(data[offset:])
-        return elements
-    if tag == BINARY_EXT:
-        length = struct.unpack(">I",data[0:4])[0]
-        return data[4:4+length]
-
-    raise Exception("Unknown erlang term type (tag: {0})".format(tag))
+    return _binary_to_term_len(bin)[0]
